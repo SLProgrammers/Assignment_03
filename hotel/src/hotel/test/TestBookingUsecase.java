2543e9dcad789c10855c42a750071347ec537ccc
Kosalaj <41555477+Kosalaj@users.noreply.github.com>
2018-09-18 13:33:50 +1000

    Testing_phoneNumberEntered_Method() unit tested

---
+++ hotel/src/hotel/test/TestBookingUsecase.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.test;
+
+import hotel.booking.BookingCTL;
+import hotel.entities.Hotel;
+import hotel.utils.IOUtils;
+
+/**
+ *
+ * @author Kosala
+ */
+public class TestBookingUsecase {
+
+    private static Hotel hotel;
+
+    public static void main(String[] args) {
+
+        Testing_phoneNumberEntered_Method();
+
+    }
+
+    private static void Testing_phoneNumberEntered_Method() {
+        hotel=new Hotel();
+        IOUtils.outputln("\nBooking Room\n");
+	new BookingCTL(hotel).run();
+    }
+}
